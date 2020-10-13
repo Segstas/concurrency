73bc851b7ab3b2ba2a11c889bf3dd9eb34c20a5f
Segstas <segstas@gmail.com>
2020-10-13 12:07:20 +0300

    init

---
+++ src/main/java/exercise93/SimpleReadWriteLock.java
@@ -0,0 +1,125 @@
+package exercise93;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class SimpleReadWriteLock implements ReadWriteLock {
+    private boolean writer;
+    private int readers;
+    private Lock readLock;
+    private Lock writeLock;
+    private final Object condition = new Object();
+    private final Object lock = new Object();
+
+    public Lock readLock() {
+        return readLock;
+    }
+
+    public Lock writeLock() {
+        return writeLock;
+    }
+
+    public SimpleReadWriteLock() {
+        this.writer = false;
+        this.readers = 0;
+        this.readLock = new ReadLock();
+        this.writeLock = new WriteLock();
+    }
+
+    private class WriteLock implements Lock {
+        public void lock() {
+            synchronized (lock) {
+                while (writer) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                writer = true;
+                while (readers > 0) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        public void unlock() {
+            synchronized (lock) {
+                writer = false;
+                lock.notifyAll();
+            }
+        }
+
+        public void lockInterruptibly() {
+            // not implemented
+        }
+
+        public boolean tryLock() {
+            // not implemented
+            return false;
+        }
+
+        public boolean tryLock(long time, TimeUnit unit) {
+            // not implemented
+            return false;
+        }
+
+        public Condition newCondition() {
+            // not implemented
+            return null;
+        }
+    }
+
+    private class ReadLock implements Lock {
+
+
+        public void unlock() {
+            synchronized (lock) {
+                readers--;
+                if (readers == 0)
+                    lock.notifyAll();
+            }
+        }
+
+        @Override
+        public void lock() {
+            {
+                synchronized (lock) {
+                    while (writer) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    readers++;
+                }
+            }
+        }
+
+        public void lockInterruptibly() {
+            // not implemented
+        }
+
+        public boolean tryLock() {
+            // not implemented
+            return false;
+        }
+
+        public boolean tryLock(long time, TimeUnit unit) {
+            // not implemented
+            return false;
+        }
+
+        public Condition newCondition() {
+            // not implemented
+            return null;
+        }
+    }
+}
